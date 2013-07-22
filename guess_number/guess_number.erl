3fafd57dfa46718be728222370e06b9c3c886c31
LolleK <olle@iix3.org>
2013-07-22 14:06:26 +0200

    erlang

--- guess_number/guess_number.erl
+++ guess_number/guess_number.erl
@@ -6,18 +6,18 @@ guess_number(Solution, Num) ->
     {ok, [Guess]} = io:fread("", "~d"),
     guess_number(Guess, Solution, Num).
 
+guess_number(Attempt, Solution, _) when Attempt == Solution ->
+    io:fwrite("Correct! You have won!\n"),
+    init:stop();
 guess_number(_, Solution, Num) when Num >= 5 ->
     io:format("Haha, I won! The number was ~w~n", [Solution]),
     init:stop();
-guess_number(Attempt, Solution, Num) when Attempt > Solution ->
-    io:fwrite("Too high! Try again!\n"),
-    guess_number(Solution, Num+1);
-guess_number(Attempt, Solution, Num) when Attempt < Solution ->
-    io:fwrite("Too low! Try again!\n"),
-    guess_number(Solution, Num+1);
-guess_number(_, _, _) ->
-    io:fwrite("Correct! You have won!\n"),
-    init:stop().
+guess_number(Attempt, Solution, Num) ->
+    if
+        Attempt > Solution -> io:fwrite("Too high! Try again!\n");
+        true -> io:fwrite("Too low! Try again!\n")
+    end,
+    guess_number(Solution, Num+1).
 
 guess_number() ->
     random:seed(now()),
