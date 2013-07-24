9d70c2ba92527aa463c502464e13840c32c8fa5a
LolleK <olle@iix3.org>
2013-07-24 12:53:11 +0200

    update

--- fibonacci/fibonacci.hs
+++ fibonacci/fibonacci.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import System.Environment
+
 fibAux :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
 fibAux (x, y, 0) = (x, y, 0)
 fibAux (x, y, z) = fibAux (y, y + x, z - 1)
@@ -9,15 +11,25 @@ fibParse (x, y, z) = x
 
 fib :: Integer -> Integer
 fib x = fibParse(fibAux(0, 1, x))
+
+getArg :: [String] -> Integer
+getArg arg = read (arg !! 0) :: Integer
+
+main = do
+  arg <- System.Environment.getArgs
+  if length arg == 1 then
+    print $ fib $ getArg arg
+    else putStrLn "Usage: ./fibonacci <number>"
+
   
 -- TAIL INFO
 -- Name: Fibonacci Sequence
 -- Language: Haskell
--- Compile: :load fibonacci.hs
+-- Compile: ghc --make fibonacci.hs
 -- State: Done
 --
 -- Prints out numbers from the fibonacci sequence
 --
 --
--- Example: fib 42
+-- Example: ./fibonacci 42
 --
