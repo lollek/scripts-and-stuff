b617e16c64ccbf58eb89d9f9c9f3c2c4509f15fe
LolleK <olle@iix3.org>
2013-07-30 19:38:41 +0200

    haskell

--- wlan.hs
+++ wlan.hs
@@ -10,6 +10,22 @@ import qualified System.IO as IO
 import qualified System.IO.Error as IOE
 import qualified System.Process as SP
 
+printInColor :: String -> Xterm.Color -> IO ()
+printInColor string color = do
+  Xterm.setSGR [Xterm.SetConsoleIntensity Xterm.BoldIntensity]
+  Xterm.setSGR [Xterm.SetColor Xterm.Foreground Xterm.Vivid color]
+  putStrLn string
+  Xterm.setSGR [Xterm.Reset]
+
+raiseHaikuError :: IO ()
+raiseHaikuError = do
+  printInColor "Error" Xterm.Red
+  putStrLn $ unlines [
+    "", "Errors have occured",
+    "We won't tell you where of why",
+    "Lazy programmers"]
+  Exit.exitFailure
+
 -- Formats result from wlan0 scan
 -- Should I rewrite this nasty piece of code?
 formatCells :: [String] -> String -> [(String, String, String, String)]
@@ -30,27 +46,36 @@ formatCells accessPoints = map formatCell . drop 1 . LS.splitOn "Cell"
 -- Prints the cells from FormatCells
 printCells :: [(String, String, String, String)] -> IO ()
 printCells cellList = do
-  putStrLn $ show (length cellList) ++ " access points found: "
-  putStrLn " No - Signal - Encrypt\t- Name"
+  putStrLn $ show (length cellList) ++ " access points found \n"
+  printInColor " No - Signal - Encrypt\t- Name" Xterm.White
   mapM_ printCell cellList
+  putStrLn ""
   where 
     printCell (num, quality, enc, essid) = do
       case enc of
-        "0" -> do
-          setXtermColor Xterm.Red
-          putStrLn $ num ++ " - " ++ quality ++ "%    - on\t- " ++ essid
-        "1" -> do
-          setXtermColor Xterm.Blue
-          putStrLn $ num ++ " - " ++ quality ++ "%    - off\t- " ++ essid
-        _ -> do
-          setXtermColor Xterm.Green
-          putStrLn $ num ++ " - " ++ quality ++ "%    - known\t- " ++ essid
-      Xterm.setSGR [Xterm.Reset]
+        "0" -> printInColor (num ++ " -   " ++ quality ++ "%  -   on\t- " ++ essid) Xterm.Red
+        "1" -> printInColor (num ++ " -   " ++ quality ++ "%  -   off\t- " ++ essid) Xterm.Blue
+        _ -> printInColor (num ++ " -   " ++ quality ++ "%  -  known\t- " ++ essid) Xterm.Green
+  
+connectTo :: (String, String, String, String) -> IO ()  
+connectTo (num, quality, "2", essid) = do
+  -- Configure wpa
+  putStrLn "Configuring wpa ... "
+  (wpaCode, _, _) <- SP.readProcessWithExitCode "wpa_supplicant"
+         ["-B", "-i", "wlan0", "-c", "/root/wlan/" ++ essid] []
+  case wpaCode of
+    Exit.ExitFailure _ -> raiseHaikuError
+    _ -> printInColor "OK" Xterm.Green
       
-      where setXtermColor color = do
-              Xterm.setSGR [Xterm.SetColor Xterm.Foreground Xterm.Vivid color]
-              Xterm.setSGR [Xterm.SetConsoleIntensity Xterm.BoldIntensity]
-
+  -- Run dhclient
+  putStrLn "Asking kindly for an IP address ... "
+  (dhCode, _, _) <-SP.readProcessWithExitCode "dhclient" ["wlan0"] []
+  case dhCode of 
+    Exit.ExitFailure _ -> raiseHaikuError
+    _ -> do
+      printInColor "OK" Xterm.Green
+      Exit.exitSuccess
+connectTo _ = raiseHaikuError      
   
 main = do
   
@@ -60,13 +85,11 @@ main = do
   
   dirScan <- E.try (Dir.getDirectoryContents "/root/wlan") :: IO (Either IOError [String])
   case dirScan of
-    
     Left err -> do 
-      putStrLn $ "[Err]\nError message: " ++ IOE.ioeGetErrorString err
+      printInColor ("Error - " ++ IOE.ioeGetErrorString err) Xterm.Red
       Exit.exitFailure
-      
     Right knownAccessPoints -> do 
-      putStrLn "[OK]"
+      printInColor "OK" Xterm.Green
   
       -- Try to check for nearby access points
       putStr "Scanning for access points ... "
@@ -74,17 +97,20 @@ main = do
 
       (exitCode, stringOfCells, _) <- SP.readProcessWithExitCode "iwlist" ["wlan0", "scan"] []
       case exitCode of
-        
         Exit.ExitFailure _ -> do
-          putStrLn "[ERR]\nAre you really signed on as root?"
-          Exit.exitFailure
-          
+          raiseHaikuError
         _ -> do
-          putStrLn "[OK]"
-          printCells $ formatCells knownAccessPoints' stringOfCells
-          where knownAccessPoints' = drop 2 $ L.sort knownAccessPoints
-
-    
+          printInColor "OK" Xterm.Green
+          printCells knownCells
+          putStrLn $ "Select WLAN to connect to (1-" ++ 
+            (show (length knownCells)) ++ ") or ^C to cancel"
+          selectedCell <- readLn :: IO Int
+          connectTo (knownCells !! pred selectedCell)
+          where
+            knownAccessPoints' = drop 2 $ L.sort knownAccessPoints 
+            knownCells = formatCells knownAccessPoints' stringOfCells
+              
+                    
 -- TAIL INFO:
 -- Name: Lollian Wlan Scan and Connect
 -- Language: Haskell
