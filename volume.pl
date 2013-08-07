511b7a3bfbcef0f03f3a95cb60b822c44030e4eb
LolleK <olle@iix3.org>
2013-08-07 17:33:11 +0200

    noop

--- volume.pl
+++ volume.pl
@@ -1,34 +1,29 @@
 #! /usr/bin/env perl
 
-# Update 2013-03-04 - Olle K
-# - Remade script from bash into perl
-# Update 2013-07-12 - Olle K
-# - Script stopped working - changed $vol[32] to $vol[31]
-
 use 5.14.2;
 use strict;
 use warnings;
 
 sub vol_get {
     my @vol = split(/ /, qx(amixer sget Master | xargs));
-    print("$vol[31]\n");
+    print("$vol[20]\n");
 }
 sub vol_set {
     my @vol = split(/ /, qx(amixer sset Master $ARGV[0]% | xargs));
-    print("$vol[31]\n");
+    print("$vol[20]\n");
 }
 sub vol_pp {
     my @vol = split(/ /, qx(amixer sset Master 10%+ | xargs));
-    print("$vol[31]\n");
+    print("$vol[20]\n");
 }
 sub vol_mm {
     my @vol = split(/ /, qx(amixer sset Master 10%- | xargs));
-    print("$vol[31]\n");
+    print("$vol[20]\n");
 }
 sub vol_mute {
     my @vol = split(/ /, qx(amixer sget Master | xargs));
-    if ($vol[32] eq "[on]\n") { @vol = qx(amixer sset Master mute); }
-    elsif ($vol[32] eq "[off]\n") { @vol = qx(amixer sset Master unmuted); }
+    if ($vol[22] eq "[on]\n") { @vol = qx(amixer sset Master mute); }
+    elsif ($vol[22] eq "[off]\n") { @vol = qx(amixer sset Master unmuted); }
     else { print("Vol is " . $vol[32] . ", thus dunno what to do"); }
 }
         
@@ -52,10 +47,10 @@ else {
 Name: Volume
 Language: Perl
 State: Done
+Created: 2013-03-04
+Updated: 2013-08-07
 
 Change the volume (through amixer)
-
-
 
 Example: ./volume.pl 100
 =cut
