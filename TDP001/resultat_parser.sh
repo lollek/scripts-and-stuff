d7c3096f76fd8ac9b9ce07dada61dbddd7b50619
Olle K <olle@iix3.org>
2013-08-28 18:54:08 +0200

    move split_results -> TDP001

--- TDP001/resultat_parser.sh
+++ TDP001/resultat_parser.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 #
 # This file will take the names and test results from resultat.txt
 #  and add their points together, sort them, and print them out
@@ -12,15 +12,15 @@ fi
 
 case $lang in 
     "perl")
-        perl -ane 'printf "%d %s %s \n", $F[2]+$F[3]+$F[4],$F[0],$F[1]' resultat.txt|sort -r
+        perl -ane 'printf "%d %s %s \n", $F[2]+$F[3]+$F[4], $F[0], $F[1]' resultat.txt | sort -r
         ;;
     "awk")
         #awk '{print$3+$4+$5,$1,$2}' resultat.txt|sort -r
-        #awk '{print$3+$4+$5,$1,$2|"sort -r"}' resultat.txt
-
-        # This needs gawk instead of awk, but that's kinda standard anyways
-        #awk '$0=$3+$4+$5" "$1" "$2' resultat.txt|sort -r
+        # This does not work in 'real' awk, but in gawk (but should work)
         awk '$0=$3+$4+$5FS$1FS$2' resultat.txt|sort -r
+        ;;
+    'bash')
+        sed 's/\ -/0/g' resultat.txt|while read -a r;do echo $[r[2]+r[3]+r[4]] ${r[0]} ${r[1]};done|sort -r
         ;;
     *)
         echo "Usage: $0 <lang>"
