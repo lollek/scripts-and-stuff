d7d4ea905fcbb33ee5c8acdfa4fb479425fb25b0
Olle K <olle@iix3.org>
2013-09-10 12:22:08 +0200

    Forgot to add this

---
+++ fibonacci/fibonacci.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+fib() {
+    old_val=1
+    new_val=0
+    tmp=0
+    r=$1
+
+    while [ $r -gt 0 ]; do
+        tmp=$new_val
+        new_val=$(( old_val + new_val ))
+        old_val=$tmp
+        r=$(( $r - 1 ))
+    done
+    echo $new_val
+}
+
+if [ -z $1 ]; then
+    for i in `seq 1 20`; do fib $i; done
+else
+    fib $1
+fi
+    
