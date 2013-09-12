b34e8c9610c0f499255f8a43b30de9f710102114
Olle K <olle@iix3.org>
2013-09-12 10:39:14 +0200

    Gold plating

--- game_of_life/game_of_life.c
+++ game_of_life/game_of_life.c
@@ -4,38 +4,35 @@
 #include <time.h>
 #include <curses.h>
 
-int init_random_map(int** map, int maxx, int maxy) {
+/* malloc a map and fill it with random numbers */
+int init_random_map(int** map, int maxyx) {
   
-  int i, *mapp;
-  int maxyx = maxy * maxx;
-
-  /* Malloc a map: */
   if (*map != NULL) {
     fprintf(stderr, "map already malloc'd\n");
     free(*map);
     return 1;
   }
 
-  *map = (int *)malloc(sizeof(int) * (maxyx));
+  *map = (int *)malloc(sizeof(int) * maxyx);
   if (*map == NULL) {
     fprintf(stderr, "Failed to malloc map\n");
     return 1;
   }
   
   /* Fill map with 1's and 0's: (>1 will later be treated as a 0) */
-  for (i = 0, mapp = map[0]; i < (maxyx); i++, mapp++)
+  for (int i = 0, *mapp = map[0]; i < (maxyx); i++, mapp++)
     *mapp = (rand() % 5);
   
   return 0;
 }
 
+/* Draw a map to screen (1s are white, 0s are black) */
 int draw_screen(int **map, int maxx, int maxy) {
   
   int err = 0;
-  int i, *mapp;
   int maxyx = maxy * maxx;
 
-  for (i = 0, mapp = map[0]; i < maxyx; i++, mapp++) {
+  for (int i = 0, *mapp = map[0]; i < maxyx; i++, mapp++) {
     if (*mapp == 1) {
       attron(A_REVERSE);
       err += mvaddch(i / maxx, i % maxx, ' ');
@@ -57,14 +54,11 @@ int wrap_around(int num, int maxyx) {
 
 void evolve(int **mapfrom, int **mapto, int maxx, int maxy) {
   
-  int i, *mappf, *mappt, nabo;
   int maxyx = maxx * maxy;
 
-  for (i = 0, mappf = mapfrom[0], mappt = mapto[0]; 
-       i < maxyx; 
-       i++, mappf++, mappt++) {
-
-    nabo = 0;
+  for (int i = 0, *mappf = mapfrom[0], *mappt = mapto[0]; 
+       i < maxyx; i++, mappf++, mappt++) {
+    int nabo = 0;
 
     if (*(mapfrom[0] + wrap_around(i -maxx -1, maxyx)) == 1) nabo++;
     if (*(mapfrom[0] + wrap_around(i -maxx, maxyx)) == 1) nabo++;
@@ -83,40 +77,40 @@ void evolve(int **mapfrom, int **mapto, int maxx, int maxy) {
 
 int main(int argc, char* argv[]) {
 
-  int err = 0, i = 0;
-  int maxx, maxy, c;
-  int *stdmap = NULL, *auxmap = NULL;
-  int *mapp1 = NULL, *mapp2 = NULL;
-
   /* Check that argv is fine: */
   if (argc != 3) {
     printf("Usage: %s <width> <height>\n", argv[0]);
     return 1;
   }
-  if ((maxx = atoi(argv[1])) < 1) {
-    fprintf(stderr, "Err: Width is %d\n", maxx);
-    return 1;
-  }
-  if ((maxy = atoi(argv[2])) < 1) {
-    fprintf(stderr, "Err: Height is %d\n", maxy);
-    return 1;
-  }
-
+  initscr();
+  curs_set(0);
+  
+  /* If user gives strange width/height, we'll just set it to max */
+  int maxy, maxx;
+  getmaxyx(stdscr, maxy, maxx);
+  int tmp_maxx = atoi(argv[1]);
+  int tmp_maxy = atoi(argv[2]);
+ 
+  if (0 < tmp_maxx && tmp_maxx < --maxx)
+    maxx = tmp_maxx;
+  if (0 < tmp_maxy && tmp_maxy < --maxy)
+    maxy = tmp_maxy;
+ 
   /* Start random-number-generator: */
   srand(time(NULL));
 
   /* Init random map: */
-  if (init_random_map(&stdmap, maxx, maxy) != 0)
+  int *stdmap = NULL, *auxmap = NULL;
+  if (init_random_map(&stdmap, maxx*maxy) != 0)
     return 1;
-  if (init_random_map(&auxmap, maxx, maxy) != 0)
+  if (init_random_map(&auxmap, maxx*maxy) != 0)
     return 1;
   
-  initscr();
-  curs_set(0);
-  err += draw_screen(&stdmap, maxx, maxy);
+  int err = draw_screen(&stdmap, maxx, maxy);
 
   timeout(100);
-  while ((c = getch()) != 'q') {
+  int i = 0, *mapp1 = NULL, *mapp2 = NULL;
+  for (int c = getch(); c != 'q'; c = getch()) {
     if (i++ % 2) {
       mapp1 = stdmap;
       mapp2 = auxmap;
@@ -141,7 +135,7 @@ int main(int argc, char* argv[]) {
 Name: Game of Life
 Language: C
 State: Done
-Compile: cc game_of_life.c -O3 -lcurses -o game_of_life
+Compile: cc game_of_life.c -std=c99 -O3 -lcurses -o game_of_life
 
 Display randomized game of life
 
