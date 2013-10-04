ab7da2e989ab0c432258a04e4508d75a2d28f8d8
Olle K <olle@iix3.org>
2013-10-04 16:22:06 +0200

    lua fibonacci

---
+++ fibonacci/fibonacci.lua
@@ -0,0 +1,21 @@
+#! /usr/bin/env lua
+
+function fib(rotations)
+  local old_value = 1
+  local new_value = 0
+  local tmp
+
+  for i = rotations, 0, -1 do
+    new_value, old_value = new_value + old_value, new_value
+  end
+
+  return new_value
+end
+
+if #arg == 1 then
+  print(fib(arg[1]))
+else
+  for i = 1,10 do
+    print(fib(i) .. "\t" .. fib(i+10))
+  end
+end
