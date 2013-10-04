ce7aa770f754629860ab765a14f9eca17ecf8a61
Olle K <olle@iix3.org>
2013-10-04 16:54:24 +0200

    added lua to guess number

---
+++ guess_number/guess_number.lua
@@ -0,0 +1,33 @@
+#! /usr/bin/env lua
+
+math.randomseed(os.time())
+local solution = math.floor(math.random(1, 100))
+
+print [[
+Guess-a-number game!
+I'm thinking of a number between 1 and 100,
+you have 5 tries to guess it correctly.
+What's your guess?
+]]
+
+local guess, guess_num = 0, 0
+while 1 do
+  guess_num = guess_num + 1
+  print(string.format("Guess %d: ", guess_num))
+  guess = io.read("*number")
+
+  if guess < solution then
+    print "Too low! Try again!"
+  elseif guess > solution then
+    print "Too high! Try again!"
+  elseif guess == solution then
+    print "Correct! You won!"
+    os.exit()
+  end
+
+  if guess_num > 4 then
+    print(string.format("Haha, I won! The number was %d!", solution))
+    os.exit()
+  end
+end
+  
