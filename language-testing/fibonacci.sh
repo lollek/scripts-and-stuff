2e9fb9ed0433b8c01b43b1b18391f555adb01130
Olle K <olle.kvarnstroem@gmail.com>
2013-12-25 11:21:26 +0100

    fixxy

--- language-testing/fibonacci.sh
+++ language-testing/fibonacci.sh
@@ -1,25 +1,27 @@
 #! /bin/sh
 
 fib() {
-    old_val=1
-    new_val=0
-    tmp=0
-    r=$1
+  old_val=1
+  new_val=0
+  tmp=0
 
-    while [ $r -gt 0 ]; do
-        tmp=$new_val
-        new_val=$(( old_val + new_val ))
-        old_val=$tmp
-        r=$(( $r - 1 ))
-    done
-    echo $new_val
+  for ((i=0; i < $1; i++)); do
+    tmp=$new_val
+    new_val=$(( old_val + new_val ))
+    old_val=$tmp
+  done
+  echo $new_val
 }
 
-if [ -z $1 ]; then
-    for i in `seq 1 20`; do fib $i; done
+if [[ -z $1 ]]; then
+  range="$(seq 1 20)"
 else
-    fib $1
+  range="$1"
 fi
+
+for num in $range; do
+  fib "$num"
+done
     
 ## Tail info:
 # Name: Fibonacci Sequence
