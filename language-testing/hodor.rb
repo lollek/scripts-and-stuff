271fed838fd6290d293baba1cfa5f368e66f353c
Olle K <olle.kvarnstroem@gmail.com>
2014-01-17 07:56:38 +0100

    hodor for ruby

---
+++ language-testing/hodor.rb
@@ -0,0 +1,15 @@
+#! /usr/bin/env ruby
+# This application replaces all words with hodor.
+# Usage: ./hodor.rb 'hello world!'
+# Usage: echo "hello world!" | ./hodor.rb
+
+def hodor(str)
+  str.gsub(/[a-zA-Z0-9]+/, 'hodor')
+end
+
+if ARGV.length > 0
+  puts hodor(ARGV.join(" "))
+else
+  $stdin.each do |line| puts hodor(line) end
+end
+
