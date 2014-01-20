d5034a6e0c476b333d7127cc0d490f4404ccd8ed
Olle K <olle.kvarnstroem@gmail.com>
2014-01-20 19:40:46 +0100

    rust fibs

---
+++ language-testing/fibonacci.rs
@@ -0,0 +1,27 @@
+// Written in rust 0.9
+// Jan 20 2014
+
+fn fib(rot: int) -> int {
+  let mut old_value = 1;
+  let mut new_value = 0;
+  for _ in range(0, rot) {
+    let tmp = old_value;
+    old_value = new_value;
+    new_value += tmp;
+  }
+  new_value
+}
+
+fn main() {
+  let arg: ~[~str] = std::os::args();
+  if (arg.len() > 1) {
+    match from_str::<int>(arg[1]) {
+      Some(x) => println!("{}", fib(x)),
+      None    => println("Bad argument!")
+    }
+  } else {
+    for r in range(0,10) {
+      println!("{}\t{}", fib(r), fib(r+10))
+    }
+  }
+}
