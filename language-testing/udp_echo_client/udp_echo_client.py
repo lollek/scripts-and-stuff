cfcafa72f88dbcf2bc2905c676a89c9cc4e258de
Olle K <olle.kvarnstroem@gmail.com>
2014-02-01 13:15:44 +0100

    python udp echo client

---
+++ language-testing/udp_echo_client/udp_echo_client.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+import sys
+import socket
+import argparse
+
+def send_receive_data(hostname, port, content):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.sendto(content.encode(), (hostname, port))
+        data, client = sock.recvfrom(1024)
+        print(data.decode(), end="\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="UDP Echo Client")
+    parser.add_argument("hostname", help="target hostname")
+    parser.add_argument("port", type=int, help="target port")
+    args = parser.parse_args()
+    send_receive_data(args.hostname, args.port, sys.stdin.read())
