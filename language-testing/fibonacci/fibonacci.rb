60e1e70f0557b37ebb1e8a91221f3e4fc5b0c981
Olle K <olle.kvarnstroem@gmail.com>
2014-02-25 13:33:03 +0100

    cleaned up

--- language-testing/fibonacci/fibonacci.rb
+++ language-testing/fibonacci/fibonacci.rb
@@ -1,37 +1,28 @@
 #! /usr/bin/env ruby
 
 def fib(rot)
-  
   old_value = 1
   new_value = 0
 
-  while rot > 0 do
+  while rot > 0
     old_value, new_value = new_value, new_value + old_value
     rot -= 1
   end
 
-  return new_value
-  
+  new_value
 end
 
-# If no arguments, run 20 fibs:
-if ARGV.length == 0 then
-  (0...10).each {|i| puts "#{fib(i)}\t#{fib(i + 10)}"}
-
-# Else, fib the first argument:
-else
-  puts fib(ARGV[0].to_i)
-
+# If no arguments; run 20 fibs, else; fib the first argument:
+if ARGV.length == 0 then (0...10).each { |i| puts "#{fib(i)}\t#{fib(i + 10)}" }
+else                     puts fib(ARGV[0].to_i)
 end
 
-=begin
-Name: Fibonacci Sequence
-Language: Ruby
-State: Done
-
-Prints out numbers from the fibonacci sequence
-
-
-Example: ./fibonacci.rb
-Example2: ./fibonacci.rb 42
-=end
+# Name: Fibonacci Sequence
+# Language: Ruby
+# State: Done
+#
+# Prints out numbers from the fibonacci sequence
+#
+#
+# Example: ./fibonacci.rb
+# Example2: ./fibonacci.rb 42
