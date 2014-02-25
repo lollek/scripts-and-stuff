641edd92e99ff977e24c7ae12c98f4cecde76a3c
Olle K <olle.kvarnstroem@gmail.com>
2014-02-25 13:38:24 +0100

    cleanup

--- language-testing/guess_number/guess_number.rb
+++ language-testing/guess_number/guess_number.rb
@@ -5,39 +5,34 @@ $stdout.sync = true
 target_number = rand(100) + 1
 try_num = 0
 
-puts "Guess-a-number game!"
-puts "I am thinking of a number between 1 and 100. "
-puts "You have 5 tries to guess it correctly or I win."
-puts "What\'s your guess? "
+puts "Guess-a-number game!\n" \
+     "I am thinking of a number between 1 and 100.\n" \
+     "You have 5 tries to guess it correctly or I win.\n" \
+     'What\'s your guess? '
 
-
-while 1 do
-  
-  print "Guess #{try_num += 1}: "
+loop do
+  print "Guess #{ try_num += 1 }: "
   attempt = gets.to_i
-  
+
   if attempt == target_number
-    puts "Correct! You won!"
+    puts 'Correct! You won!'
     break
   elsif try_num == 5
     puts "Haha, I won! The number was #{target_number}"
     break
   elsif attempt > target_number
-    puts "Too high! Try again!"
+    puts 'Too high! Try again!'
   elsif attempt < target_number
-    puts "Too low! Try again!"
+    puts 'Too low! Try again!'
   end
-
 end
 
-=begin TAIL INFO:
-Name: Guess Number
-Language: Ruby
-State: Done
-
-Play guess-a-number game
-
-
-
-Example: ./guess_number.rb
-=end
+# Name: Guess Number
+# Language: Ruby
+# State: Done
+#
+# Play guess-a-number game
+#
+#
+#
+# Example: ./guess_number.rb
