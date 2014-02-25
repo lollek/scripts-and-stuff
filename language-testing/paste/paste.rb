fb4e91e4e3fc1af8a895b822d4553884dd4f5831
Olle K <olle.kvarnstroem@gmail.com>
2014-02-25 13:43:42 +0100

    cleanup

--- language-testing/paste/paste.rb
+++ language-testing/paste/paste.rb
@@ -1,6 +1,5 @@
 #! /usr/bin/env ruby
 
-require 'optparse'
 
 def paste(filename1, filename2, delim)
   file1 = open(filename1)
@@ -10,12 +9,11 @@ def paste(filename1, filename2, delim)
     line1 = file1.gets
     line2 = file2.gets
 
-    if line1 or line2 then
+    if line1 || line2
       print line1.chomp if line1
       print delim
       print line2.chomp if line2
       puts
-
     else
       file1.close
       file2.close
@@ -24,26 +22,28 @@ def paste(filename1, filename2, delim)
   end
 end
 
-delim = " "
-OptionParser.new do |opts|
-  opts.banner = "Usage: #{$0} file1 file2 [-d DELIM]"
-  opts.on("-d DELIM", "delimiter to separate lines") {|d| delim = d}
-end.parse!
+if __FILE__ == $PROGRAM_NAME
+  require 'optparse'
 
-if ARGV.length == 2 then
-  paste ARGV[0], ARGV[1], delim
-else
-  puts "Bad arguments, see #{$0} -h"
+  delim = ' '
+  OptionParser.new do |opts|
+    opts.banner = "Usage: #{$PROGRAM_NAME} file1 file2 [-d DELIM]"
+    opts.on('-d DELIM' , 'delimiter to separate lines') { |d| delim = d }
+  end.parse!
+
+  if ARGV.length == 2
+    paste ARGV[0], ARGV[1], delim
+  else
+    puts "Bad arguments, see #{$PROGRAM_NAME} -h"
+  end
 end
 
-=begin TAIL INFO
-Name: Paste
-Language: Ruby
-State: Done
-
-This is an answer to a python-test question at my school, basically:
-It should take two files as argument and paste them with delim in between
-It should be possible to change the delimiter with -d
-The files must be read line-by-line
-Example: ./paste.rb A.TXT B.TXT -d1
-=end
+# Name: Paste
+# Language: Ruby
+# State: Done
+#
+# This is an answer to a python-test question at my school, basically:
+# It should take two files as argument and paste them with delim in between
+# It should be possible to change the delimiter with -d
+# The files must be read line-by-line
+# Example: ./paste.rb A.TXT B.TXT -d1
