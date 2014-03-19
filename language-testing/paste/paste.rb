c736226b8ba9cd12a87cf4431f90d09e7d2c4f50
Olle K <olle.kvarnstroem@gmail.com>
2014-03-19 13:38:54 +0100

    minor changes

--- language-testing/paste/paste.rb
+++ language-testing/paste/paste.rb
@@ -2,40 +2,37 @@
 
 
 def paste(filename1, filename2, delim)
-  file1 = open(filename1)
-  file2 = open(filename2)
-
-  while 1
-    line1 = file1.gets
-    line2 = file2.gets
-
-    if line1 || line2
-      print line1.chomp if line1
-      print delim
-      print line2.chomp if line2
-      puts
-    else
-      file1.close
-      file2.close
-      break
+  File.open filename1 do |file1|
+    File.open filename2 do |file2|
+      line1, line2 = file1.gets, file2.gets
+      while line1 || line2
+        puts "#{line1.chomp if line1}#{delim}#{line2.chomp if line2}"
+        line1, line2 = file1.gets, file2.gets
+      end
     end
   end
+end
+
+def usage
+  $stderr.puts "Bad arguments, see #{$PROGRAM_NAME} -h"
+  exit 1
 end
 
 if __FILE__ == $PROGRAM_NAME
   require 'optparse'
 
   delim = ' '
-  OptionParser.new do |opts|
-    opts.banner = "Usage: #{$PROGRAM_NAME} file1 file2 [-d DELIM]"
-    opts.on('-d DELIM' , 'delimiter to separate lines') { |d| delim = d }
-  end.parse!
-
-  if ARGV.length == 2
-    paste ARGV[0], ARGV[1], delim
-  else
-    puts "Bad arguments, see #{$PROGRAM_NAME} -h"
+  begin
+    OptionParser.new do |opts|
+      opts.banner = "Usage: #{$PROGRAM_NAME} file1 file2 [-d DELIM]"
+      opts.on('-d DELIM' , 'delimiter to separate lines') { |d| delim = d }
+    end.parse!
+  rescue
+    usage
   end
+
+  usage unless ARGV.length == 2
+  paste ARGV[0], ARGV[1], delim
 end
 
 # Name: Paste
