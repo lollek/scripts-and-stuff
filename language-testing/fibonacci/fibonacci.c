e0389d18d55998c29447f4bac256238287baf850
Olle K <olle.kvarnstroem@gmail.com>
2014-03-20 21:59:48 +0100

    added test cases

--- language-testing/fibonacci/fibonacci.c
+++ language-testing/fibonacci/fibonacci.c
@@ -1,13 +1,15 @@
-/* Compile with -std=c99 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 unsigned long fib(int rot) {
   unsigned long old_value = 1;
   unsigned long new_value = 0;
   unsigned long tmp;
+  int i;
 
-  while (--rot >= 0) {
+  for (i = 0; i < rot; ++i) {
     tmp = old_value;
     old_value = new_value;
     new_value = tmp + old_value;
@@ -15,15 +17,29 @@ unsigned long fib(int rot) {
   return new_value;
 }
 
+int test() {
+  puts("Starting test cases");
+  assert(fib(0) == 0);
+  assert(fib(1) == 1);
+  assert(fib(2) == 1);
+  assert(fib(42) == 267914296);
+  puts("Success!");
+  return 0;
+}
+
 int main(int argc, char* argv[]) {
+  int i;
 
   /* Fib the argument, if possible: */
   if (argc == 2) {
-    printf("%lu\n", fib(atoi(argv[1])));
-
+    if (!strcmp(argv[1], "test")) {
+      return test();
+    } else {
+      printf("%lu\n", fib(atoi(argv[1])));
+    }
   /* Else: run 20 fibs: */
   } else {
-    for (int i = 0; i < 10; i++) {
+    for (i = 0; i < 10; ++i) {
       printf("%lu\t%lu\n", fib(i), fib(i+10));
     }
   }
