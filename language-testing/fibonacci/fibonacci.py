16202fc427805d8d8cffebda99cea3f266e824ad
Olle K <olle.kvarnstroem@gmail.com>
2014-03-20 21:34:02 +0100

    added test cases

--- language-testing/fibonacci/fibonacci.py
+++ language-testing/fibonacci/fibonacci.py
@@ -2,26 +2,34 @@
 
 from sys import argv
 
+def test():
+    print("Running test-cases")
+    try:
+        assert fib(0) == 0
+        assert fib(1) == 1
+        assert fib(2) == 1
+        assert fib(42) == 267914296
+    except:
+        print("Failure!")
+        raise
+    print("Success!")
+    exit(0)
+
 def fib(rot):
-    
     old_value = 1
     new_value = 0
-
-    while rot > 0:
-        
+    for _ in range(rot):
         old_value, new_value = new_value, new_value + old_value
-        rot -= 1
-
     return new_value
 
-# If no arguments, print 20 fibs::
-if len(argv) == 1:
+# If no arguments, print 20 fibs
+# Otherwise we'll fib the first argument
+
+if not len(argv) == 1:
+    test() if argv[1] == "test" else print(fib(int(argv[1])))
+else:
     for r in range(10):
         print("%s\t%s" % (fib(r), fib(r+10)))
-
-# Otherwise we'll fib the first argument:
-else:
-    print(fib(int(argv[1])))
 
 """ TAIL INFO:
 Name: Fibonacci Sequence
