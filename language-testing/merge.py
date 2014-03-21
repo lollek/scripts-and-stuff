a2af659dd5f1b6826f2431eb2f9eeb7e0dc97a0d
Olle K <olle.kvarnstroem@gmail.com>
2014-03-21 20:56:29 +0100

    added test-cases

--- language-testing/merge.py
+++ language-testing/merge.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 
 def merge(list1, list2):
     small, large = sorted([list1[::-1], list2[::-1]], key=lambda x: len(x))
@@ -16,6 +17,17 @@ def merge(list1, list2):
 
     return new_list
 
+def test():
+    print("Starting test-cases")
+    assert merge([1, 2, 3, 4],['a', 'b',  'c']) == [1, 'a', 2, 'b', 3, 'c', 4]
+    assert merge([1, 2, 3, 4, 5], ['a', 'b']) == [1, 2, 'a', 3, 4, 'b', 5]
+    assert merge(['a', 'b', 'c'], [1, 2, 3, 4, 5]) == [1, 'a', 2, 'b', 3, 'c', 4, 5]
+    assert merge([1, 2, 3, 4, 5, 6], ['a', 'b']) == [1, 2, 'a', 3, 4, 'b', 5, 6]
+    assert merge(list(range(1,9)), ['A', 'B', 'C', 'D', 'E']) == [1, 'A', 2, 'B', 3, 'C', 4, 'D', 5, 'E', 6, 7, 8]
+    print("Success!")
+
+if __name__ == "__main__":
+    test()
 
 """ TAIL INFO:
 Name: Merge
