60c6ebd3e73f18066ca6d629c0e772a4acd83f35
Olle K <olle.kvarnstroem@gmail.com>
2014-03-21 14:34:49 +0100

    added paste for clojure

---
+++ language-testing/paste/paste.clj
@@ -0,0 +1,25 @@
+(defn paste [filename1 filename2 delim]
+  (defn safe-head [x]
+    (if (nil? x) "" (first x)))
+  (with-open [file1 (clojure.java.io/reader filename1)
+              file2 (clojure.java.io/reader filename2)]
+    (loop [line1 (line-seq file1)
+           line2 (line-seq file2)]
+      (if (or line1 line2)
+        (do
+          (printf "%s%s%s\n" (safe-head line1) delim (safe-head line2))
+          (recur (next line1) (next line2)))))))
+
+(defn parse-args [a b c]
+  (cond
+    (.equals (subs a 0 2) "-d") (paste b c (subs a 2))
+    (.equals (subs b 0 2) "-d") (paste a c (subs b 2))
+    (.equals (subs c 0 2) "-d") (paste a b (subs c 2))
+    :else (println "Usage: ./paste FILE1 FILE2 [-d DELIM]")))
+
+(let [argv *command-line-args*
+      argc (count argv)]
+  (cond
+    (= argc 2) (paste (nth argv 0) (nth argv 1) " ")
+    (= argc 3) (parse-args (nth argv 0) (nth argv 1) (nth argv 2))
+    :else (println "Usage: ./paste FILE1 FILE2 [-d DELIM]")))
