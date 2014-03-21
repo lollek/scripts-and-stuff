968e3e4220255e853d65b66dbacec1ff0dd18a4a
Olle K <olle.kvarnstroem@gmail.com>
2014-03-21 14:38:27 +0100

    made code a bit cleaner

--- language-testing/guess_number/guess_number.clj
+++ language-testing/guess_number/guess_number.clj
@@ -12,12 +12,11 @@
     (print "Guess" guess_num ": ")
     (flush)
     (let [guess (read-string (read-line))]
-      (if (= guess solution)
-        (println "Correct! You have won!")
-        (if (= guess_num 5)
-          (println "Haha, I won! The number was" solution)
-          (do
-            (if (< guess solution)
-              (println "Too low! Try again!")
-              (println "Too high! Try again!"))
-            (recur (inc guess_num))))))))
+      (cond
+        (= guess solution) (println "Correct! You have won!")
+        (= guess_num 5) (println "Haha, I won! The number was" solution)
+        :else (do
+                (if (< guess solution)
+                  (println "Too low! Try again!")
+                  (println "Too high! Try again!"))
+                (recur (inc guess_num)))))))
