11b888381f384f56c715fd7026ee1b0ca7732903
Olle K <olle.kvarnstroem@gmail.com>
2014-03-24 14:13:53 +0100

    fixed trailing parentesis

--- language-testing/merge.clj
+++ language-testing/merge.clj
@@ -19,6 +19,5 @@
   (println (merge_ [1 2 3 4 5] [\a \b]))
   (println (merge_ [\a \b \c] [1 2 3 4 5]))
   (println (merge_ [1 2 3 4 5 6] [\a \b]))
-  (println (merge_ [1 2 3 4 5 6 7 8] [\a \b \c \d \e]))
-  )
+  (println (merge_ [1 2 3 4 5 6 7 8] [\a \b \c \d \e])))
 (test-merge_)
