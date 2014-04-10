eec4e2f08569ef05e1ab1880a0f458da7919310a
Olle K <olle.kvarnstroem@gmail.com>
2014-04-10 07:49:24 +0200

    changed name of functions

--- language-testing/guess_number/guess_number.erl
+++ language-testing/guess_number/guess_number.erl
@@ -1,35 +1,34 @@
 -module(guess_number).
 -export([guess_number/0]).
 
-take_guess(Solution, Num) ->
-    io:format("Guess ~w: ", [Num +1]),
-    {ok, [Guess]} = io:fread("", "~d"),
-    check_guess(Guess, Solution, Num +1).
-
-check_guess(Solution, Solution, _) ->
+guess(Solution, Solution, _) ->
     io:fwrite("Correct! You have won!\n"),
     init:stop();
-check_guess(_, Solution, 5) ->
+guess(_, Solution, 5) ->
     io:format("Haha, I won! The number was ~w~n", [Solution]),
     init:stop();
-check_guess(Guess, Solution, Num) when Guess > Solution ->
+guess(Guess, Solution, Num) when Guess > Solution ->
     io:fwrite("Too high! Try again!\n"),
-    take_guess(Solution, Num);
-check_guess(Guess, Solution, Num) when Guess < Solution ->
+    guess(Solution, Num +1);
+guess(Guess, Solution, Num) when Guess < Solution ->
     io:fwrite("Too low! Try again!\n"),
-    take_guess(Solution, Num).
+    guess(Solution, Num +1).
+
+guess(Solution, Num) ->
+    io:format("Guess ~w: ", [Num]),
+    {ok, [Guess]} = io:fread("", "~d"),
+    guess(Guess, Solution, Num).
 
 guess_number() ->
     random:seed(now()),
     Solution = random:uniform(100),
-    
+
     io:fwrite("Guess-a-number game!\n"),
     io:fwrite("I am thinking of a number between 1 and 100.\n"),
     io:fwrite("You have 5 tries to guess it correctly or I win.\n"),
     io:fwrite("What's your guess?\n"),
-    take_guess(Solution, 0).
+    guess(Solution, 1).
 
-    
 % TAIL INFO:
 % Name: Guess Number
 % Language: Erlang
