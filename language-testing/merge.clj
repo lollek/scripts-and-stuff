37b58e1285e9187a0841f6bf2f4ac42452814511
Olle K <olle.kvarnstroem@gmail.com>
2014-05-26 14:55:42 +0200

    mini-change

--- language-testing/merge.clj
+++ language-testing/merge.clj
@@ -1,15 +1,13 @@
 (defn merge_ [list1 list2]
   (defn private_merge [large small ratio results]
-    (if (and (empty? large) (empty? small))
-      results
-      (private_merge
-        (drop ratio large)
-        (drop 1 small)
-        ratio
-        (concat results (take ratio large) (take 1 small)))))
+    (if (or (seq large) (seq small))
+      (recur
+        (drop ratio large) (drop 1 small) ratio
+        (concat results (take ratio large) (take 1 small)))
+      results))
   (let [ratio (int (/ (count list1) (count list2)))]
     (if (<= ratio 0)
-      (merge_ list2 list1)
+      (recur list2 list1)
       (if (= (mod (count list1) (count list2)) 0)
         (private_merge list1 list2 (dec ratio) [])
         (private_merge list1 list2 ratio [])))))
