dc87168f9552e1efdaa9be20de43912ffbc0a00f
Olle K <olle.kvarnstroem@gmail.com>
2014-05-26 15:21:45 +0200

    made more clojure-y

--- language-testing/merge.clj
+++ language-testing/merge.clj
@@ -6,11 +6,11 @@
         (concat results (take ratio large) (take 1 small)))
       results))
   (let [ratio (int (/ (count list1) (count list2)))]
-    (if (<= ratio 0)
-      (recur list2 list1)
-      (if (= (mod (count list1) (count list2)) 0)
+    (if (pos? ratio)
+      (if (zero? (mod (count list1) (count list2)))
         (private_merge list1 list2 (dec ratio) [])
-        (private_merge list1 list2 ratio [])))))
+        (private_merge list1 list2 ratio []))
+      (recur list2 list1))))
 
 (defn test-merge_ []
   (println (merge_ [1 2 3 4] [\a \b \c]))
